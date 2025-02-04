e91f347b1d4cad96ad174f7c1013f632b05bc060
Petrik Diak <diak@petrik.hu>
2025-02-04 10:08:45 +0100

    Add project files.

---
+++ OOPalapok/Ember.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPalapok
+{
+	internal class Ember
+	{
+		private double testSuly { get; set; }
+		private int testMagassag { get; set; }
+
+		public Ember(double TestSuly, int TestMagassag)
+		{
+			testSuly = TestSuly;
+			testMagassag = TestMagassag;
+		}
+
+		public double TestTomegIndex()
+		{
+			double testindex = testSuly/Math.Sqrt(testMagassag/10);
+			testindex = Math.Round(testindex, 2);
+			return testindex;
+
+		}
+
+		public bool NormalTTI()
+		{
+			double testtomegszam = TestTomegIndex();
+			if (testtomegszam>18.5&&testtomegszam<=25)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"Testsúly: {testSuly}\nTesmagasság: {testMagassag}";
+		}
+
+
+	}
+	
+	
+
+}
---
+++ OOPalapok/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace OOPalapok
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Random random = new Random();
+			double suly = Math.Round(random.NextDouble() * (150 - 50) + 50, 1);
+			int randomMagassag = random.Next(150, 210);
+			Ember ember = new Ember(suly, randomMagassag);
+            Console.WriteLine(ember);
+            Console.WriteLine($"A testömegindex {ember.TestTomegIndex()}");
+			if(ember.NormalTTI())
+			{
+                Console.WriteLine($"A testtömeginex a normál tartományba esik: {ember.TestTomegIndex()}");
+			}
+			else
+			{
+                Console.WriteLine($"A testtömegindex nem esik bele a normál tartmányba:{ember.TestTomegIndex}");
+            }
+        }
+	}
+}

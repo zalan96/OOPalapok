90c1de857623539202b72afb6c023f41021338f2
Petrik Diak <diak@petrik.hu>
2025-02-04 10:20:23 +0100

    done

---
+++ OOPalapok/Emberek.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPalapok
+{
+	internal class Emberek
+	{
+		private int db { get; set; }
+		private List<Ember> emberek = new List<Ember>();
+
+		private Emberek(int DB)
+		{
+			db = DB;
+
+		}
+		public override string ToString()
+		{
+			return $"Testsúly (kg)\t TestMagasság (cm)\t TTI \t Testalkat";
+
+		}
+		private double randommagassag()
+		{
+			Random random = new Random();
+			double magassag = Math.Round(random.NextDouble() + 1, 2);
+			return magassag;
+
+		}
+		private double randomsuly()
+		{
+			Random random = new Random(); double suly = Math.Round(random.NextDouble() * 100);
+			return suly;
+		}
+		public void Emberkeszito(Emberek emberz)
+		{
+			for (int i = 0; i < emberz.db; i++)
+			{
+				Ember ujbacsi = new Ember(randomsuly(), randommagassag());
+				emberek.Add(ujbacsi);
+			}
+			Console.WriteLine($" {emberz.db} Bácsi hozzáadva");
+
+		}
+		public double atlagSuly()
+		{
+			List<double> temp = new List<double>();
+			for (int i = 0; i < emberek.Count; i++) { temp.Add(emberek[i].testSuly); }
+			double tempsum = temp.Sum();
+			double atlag = tempsum / temp.Count();
+			return atlag;
+
+		}
+		public double atlagMag()
+		{
+			List<double> temp = new List<double>();
+			for (int i = 0; i < emberek.Count; i++) { temp.Add(emberek[i].testMagassag); }
+			double tempsum = temp.Sum();
+			double atlag = tempsum / temp.Count();
+			return atlag;
+
+		}
+	}
+}

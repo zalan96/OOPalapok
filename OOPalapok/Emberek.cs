0d9a42071e3a43c4e20c3b776ed6aff7c995d156
zalan96 <146957249+zalan96@users.noreply.github.com>
2025-02-10 21:51:22 +0100

    Update Emberek.cs

--- OOPalapok/Emberek.cs
+++ OOPalapok/Emberek.cs
@@ -20,46 +20,6 @@ namespace OOPalapok
 		{
 			return $"Testsúly (kg)\t TestMagasság (cm)\t TTI \t Testalkat";
 
-		}
-		private double randommagassag()
-		{
-			Random random = new Random();
-			double magassag = Math.Round(random.NextDouble() + 1, 2);
-			return magassag;
-
-		}
-		private double randomsuly()
-		{
-			Random random = new Random(); double suly = Math.Round(random.NextDouble() * 100);
-			return suly;
-		}
-		public void Emberkeszito(Emberek emberz)
-		{
-			for (int i = 0; i < emberz.db; i++)
-			{
-				Ember ujbacsi = new Ember(randomsuly(), randommagassag());
-				emberek.Add(ujbacsi);
-			}
-			Console.WriteLine($" {emberz.db} Bácsi hozzáadva");
-
-		}
-		public double atlagSuly()
-		{
-			List<double> temp = new List<double>();
-			for (int i = 0; i < emberek.Count; i++) { temp.Add(emberek[i].testSuly); }
-			double tempsum = temp.Sum();
-			double atlag = tempsum / temp.Count();
-			return atlag;
-
-		}
-		public double atlagMag()
-		{
-			List<double> temp = new List<double>();
-			for (int i = 0; i < emberek.Count; i++) { temp.Add(emberek[i].testMagassag); }
-			double tempsum = temp.Sum();
-			double atlag = tempsum / temp.Count();
-			return atlag;
-
-		}
+		}	
 	}
 }
